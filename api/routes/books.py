11c6589ef23bd436dc2ff34ead6513fc328af182
Abdulazeez_A <97477565+shy-programmer@users.noreply.github.com>
2025-02-10 21:38:49 +0100

    Adding individual book param route

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,6 +1,6 @@
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
@@ -46,6 +46,14 @@ async def create_book(book: Book):
 )
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
+
+@router.get(
+    "/{book_id}", response_model=Book)
+async def get_book(book_id: int) ->  Book:
+    books = db.get_books()
+    if book_id not in books:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return db.get_book(book_id)
 
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
